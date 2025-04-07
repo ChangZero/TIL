f8e8723cd79525aab892d074597ba9c5e9aac716
ChangZero <changzero.kim@gmail.com>
2025-04-07 10:55:43 +0900

    Feat: v-For

--- FrontEnd/vue/movie-info/src/App.vue
+++ FrontEnd/vue/movie-info/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <h1>영화정보</h1>
-  <div>
-    <h3 class =  "bg-yellow" :style="textRed">{{ title }}</h3>
-    <p>개봉: {{ year }}</p>
-    <p>장르: {{ category }}</p>
+  <div v-for="(movie, i) in data" :key="i">
+    <h3 class =  "bg-yellow" :style="textRed">{{ movie.title }}</h3>
+    <p>개봉: {{ movie.year }}</p>
+    <p>장르: {{ movie.category }}</p>
+    <p v-for="(item, i) in movie.foods" :key="i">{{ item }}</p>
   </div>
 </template>
 <script>
@@ -11,10 +12,22 @@ export default {
   name: 'App',
   data(){
     return {
-      title: "노량",
-      year: 2023,
-      category: "액션, 드라마",
-      textRed: "color: red",
+      data: [
+        {
+        foods: ["김밥", "순대", "만두"],
+        title: "노량",
+        year: 2023,
+        category: "액션, 드라마",
+        textRed: "color: red",
+        },
+        {
+        foods: ["김밥", "순대", "만두"],
+        title: "아쿠아맨",
+        year: 2023,
+        category: "액션, 드라마",
+        textRed: "color: red",
+        }
+      ]
     }
   }
 }
