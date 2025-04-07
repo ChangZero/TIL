5c4ef4d1a9f8ae4659b45408996b8a3c1801b852
ChangZero <changzero.kim@gmail.com>
2025-04-07 15:11:43 +0900

    Feat: Data import

---
+++ FrontEnd/vue/movie-info/src/assets/movies.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    title: "노량",
+    year: 2023,
+    category: "액션, 드라마",
+    textRed: "color: red",
+    like: 0,
+    imgUrl: './assets/logo2.png'
+  },
+  {
+    title: "아쿠아맨과 로스트 킹덤",
+    year: 2023,
+    category: '액션, 판타지, 어드벤처',
+    like: 0,
+    imgUrl: './assets/logo1.png'
+  },
+  {
+    title: "3일의 휴가",
+    year: 2023,
+    category: '판타지, 드라마',
+    like: 0,
+    imgUrl: './assets/logo2.png'
+  },
+]
+
+export default data;
+
