2f8fde8278d89571234e41fc950f1c14575339ab
ChangZero <changzero.kim@gmail.com>
2025-04-07 10:34:42 +0900

    Setup: Vue

---
+++ FrontEnd/vue/movie-info/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>영화정보</h1>
+  <div>
+    <h3 class =  "bg-yellow" :style="textRed">{{ title }}</h3>
+    <p>개봉: {{ year }}</p>
+    <p>장르: {{ category }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      title: "노량",
+      year: 2023,
+      category: "액션, 드라마",
+      textRed: "color: red",
+    }
+  }
+}
+</script>
+
+<style>
+  .bg-yellow{
+    background: gold;
+    padding: 10px;
+  }
+</style>
